369eb5498baf12df76a69c03986d2922da21df98
andersonstu <andersonstu@hotmail.com>
2018-10-23 21:08:41 +0100

    Completed first version of the script to auto-generate AMA training data.
    Added generated qna_AMA.tsv

--- qna/generateFromAMA.R
+++ qna/generateFromAMA.R
@@ -17,12 +17,12 @@ kbLines <- read_lines('https://raw.githubusercontent.com/buildOMG/kb/master/kb.m
 question <- ''
 answer <- ''
 foundQs <- F
-df <- data.frame(Q=as.Date(character()),A=character())
+qna_q <- data.frame(Q=as.Date(character()),A=character())
 for(i in 1:length(kbLines)){
   if(grepl('^###', kbLines[i])){
     foundQs <- T
     row <- as.data.frame(cbind(as.character(question), as.character(answer)))
-    df <- rbind.data.frame(df, row)
+    qna_q <- rbind.data.frame(qna_q, row)
     question <- kbLines[i]
     answer <- ''
   } else {
@@ -31,57 +31,72 @@ for(i in 1:length(kbLines)){
     }
   }
 }
-colnames(df) <- c('question', 'answer')
+colnames(qna_q) <- c('question', 'answer')
 
 # Clean up questions
-df$question <- as.character(df$question)
-df$question <- str_replace_all(df$question,'### ', '')
-df$question <- str_replace_all(df$question,fixed("\\"), '') # Prob not necessary
-df$question <- str_squish(df$question)
+qna_q$question <- as.character(qna_q$question)
+qna_q$question <- str_replace_all(qna_q$question,'### ', '')
+qna_q$question <- str_replace_all(qna_q$question,fixed("\\"), '') # Prob not necessary
+qna_q$question <- str_squish(qna_q$question)
 # Clean up answers
-df$answer <- as.character(df$answer)
+qna_q$answer <- as.character(qna_q$answer)
 
 flog.info("Extract title and link to seperate vectors")
-xtitle <- str_extract_all(df$question, re_square)
-xlink <- str_extract_all(df$question, re_brace)
+xtitle <- str_extract_all(qna_q$question, re_square)
+xlink <- str_extract_all(qna_q$question, re_brace)
 
 flog.info("Cleanup titles and links")
 titles <- vector()
-for (i in 1:length(xtitle)){
+for (i in 2:length(xtitle)){
   titles[i] <- head(xtitle[i][[1]], 1)
 }
 links <- vector()
-for (i in 1:length(xlink)){
+for (i in 2:length(xlink)){
   url <- tail(xlink[i][[1]], 1)
-  links[i] <- paste0('(', root, str_sub(url, 2, str_length(url)-1), ')')
+  links[i] <- paste0('(', str_sub(url, 2, str_length(url)-1), ')')
 }
 
 flog.info("Add the cleaned titles and links to a data frame")
-links <- cbind(
-  xtitle
-  ,xlink
+qna_q <- cbind(
+  titles
+  ,links
 ) %>% as.data.frame()
-colnames(links) <- c('link')
-links %>% View
+colnames(qna_q) <- c('question','answer')
+qna_q %>% View
 
-df$answer <- str_replace_all(df$answer,fixed('>'), '')
-df$answer <- str_replace_all(df$answer,fixed('\\s+'), '')
-df$answer <- str_squish(df$answer)
-df$source <- 'AMA'
-df$meta <- ''
-
-xxx <- str_replace_all(df$question, '[^A-Za-z ]', '')
-xxx <- str_replace_all(xxx, ' ', '-')
-xxx[1]
-
-the-cosmos-hard-spoon-has-been-described-as-a-test-net-by-community-members-but-in-a-blog-post-you-mention-healthy-competition-can-you-clarify-this-tokens-short-term-and-long-term-purpose-i-e-where-will-volume-come-from-who-will-be-managing-promoting-it-and-is-it-something-that-will-be-supported-long-term-or-dropped-in-favor-of-plasma
-
-df$question[2]
-
+# qna_q$answer <- str_replace_all(qna_q$answer,fixed('>'), '')
+# qna_q$answer <- str_replace_all(qna_q$answer,fixed('\\s+'), '')
+# qna_q$answer <- str_squish(qna_q$answer)
+# qna_q$source <- 'AMA'
+# qna_q$meta <- ''
+# 
+# xxx <- str_replace_all(qna_q$question, '[^A-Za-z ]', '')
+# xxx <- str_replace_all(xxx, ' ', '-')
+# xxx[1]
 
 
 # Remove any dodgy, like the header row
-df <- df[str_length(df$question) > 3,]
+qna_q <- qna_q[str_length(qna_q$question) > 3,]
+qna_q$source <- 'GitBookAMA'
+qna_q$meta <- ''
+
+qna_q$question <- as.character(qna_q$question)
+qna_q$answer <- as.character(qna_q$answer)
+
+flog.info("Test generated links")
+# test all of the auto-generated links here?
+for(i in 1:length(qna_q$answer)){
+  #for(i in 1:5){
+  # url <- ex_url(qna_q$answer[i], pattern = "@rm_url3")[[1]]
+  url <- tail(str_extract_all(qna_q$answer[i], "\\([^()]+\\)")[[1]], 1)
+  url <- str_sub(url, 2, str_length(url)-1)
+  flog.info("Testing URL %s", url)
+  # TODO: Remove link if it errored
+  tryCatch(
+    !http_error(url, config(followlocation = 0L), USE.NAMES = FALSE),
+    error = function(e) flog.error("URL FAILED: %s", url)
+  )
+}
 
 # Output to TSV
-write_tsv(df, 'qna_AMA.tsv')
+write_tsv(qna_q, 'qna_AMA.tsv')
